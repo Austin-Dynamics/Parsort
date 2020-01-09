31808472739472ba5535b06151874fcce862770c
Austin-Dynamics <austin@parsonsphotography.com>
2020-01-09 15:45:43 -0800

    Face detection now falls back on a nose detection system when nothing is found. Now finding all faces in almost every set.

--- Parsort.py
+++ Parsort.py
@@ -12,10 +12,13 @@ import shutil
 #start and end of the vertical read chunk on images
 start = 0.1
 end = 0.6
-margin = 0.2
+margin = 0.35
 
-brightbasket = 0.2
-darkbasket = 0.4
+#crop amount on face-identified square
+facecrop = 0.25
+
+brightbasket = 0.15
+darkbasket = 0.6
 
 #contrast sensitivity variable
 senso = 0.15 #how much of the bright and dark of an image chunk should be averaged to get the contrast score
@@ -34,16 +37,17 @@ rootpath = askdirectory()
 if os.path.exists(rootpath):
     print("This is a real path")
 else:
-    print("This is not a real path.")
+    print("Path not selected or cannot be found.")
+    input("Press Enter to continue...")
     exit()
 
 #Check if deposit folders exist, and make them if not
-createfolders = ["Dark","High Contrast","Bright"]
+createfolders = ["Dark","High Contrast","Bright","Unsorted"]
 for folder in createfolders:
     try:
         os.mkdir(join(rootpath, folder))
     except OSError:
-        print("Folder ", folder, " already exists or cannot be made here.")
+        print("Folder ",folder," already exists or cannot be made here.")
     else:
         print(folder, "made.")
 
@@ -53,14 +57,15 @@ onlyjpegs = [imago for imago in onlyfiles if imago.endswith(".jpg")]
 print(rootpath)
 if onlyjpegs == []:
     print("This folder contains no jpeg images.")
+    input("Press Enter to continue...")
     exit()
 
 #setup values for the sorting loop
 brightlist = [] #create empty list to store brightness values
 contra = [] #create empty contrast value list
-facebrightness = []
-squarebrightness = []
+
 nofind = []
+facecaught = []
 #store dimensions of image for later use (all images should be same size for effective sort)
 dimensions = (cv2.imread(join(rootpath, onlyjpegs[0]))).shape
 ydim = dimensions[0]
@@ -76,48 +81,56 @@ for current in trange(totality):
     imagesearch = imagenow[ystart:yend, 0:xdim]
     face = face_cascade.detectMultiScale(imagesearch, 1.2, 5)
     if type(face) == np.ndarray:
-        imageface = imagesearch[(face[0,1]):(face[0,1]+face[0,3]),(face[0,0]):(face[0,0]+face[0,2])]
+        imageface = imagesearch[
+                    int(face[0,1]+((face[0,3]*facecrop))):int(face[0,1]+(face[0,3]*(1-2*facecrop))),
+                    int(face[0,0]+face[0,2]*facecrop):int(face[0,0]+(face[0,2]*(1-2*facecrop)))]
         brighto = np.mean(imageface)
         contrasttime = np.sort(np.reshape(imageface, [1, imageface.size]))
         consize = contrasttime.size
         facelist.append(onlyjpegs[current])
-        facebrightness.append(brighto)
     else:
-        imagesearch = imagenow[ystart:yend, margo:(xdim - margo)]
-        brighto = np.mean(imagesearch)
-        contrasttime = np.sort(np.reshape(imagesearch, [1,imagesearch.size]))
-        consize = contrasttime.size
-        squarebrightness.append(brighto)
-        nofind.append(onlyjpegs[current])
+        face_cascade = cv2.CascadeClassifier(join(script_dir, 'important/haarcascade_mcs_nose.xml'))
+        nose = face_cascade.detectMultiScale(imagesearch, 1.2, 5)
+        if type(nose) == np.ndarray:
+            imageface = imagesearch[
+                        int(nose[0, 1] - ((nose[0, 3] * 1))):int(nose[0, 1] + (nose[0, 3] * (2.2))),
+                        int(nose[0, 0] - nose[0, 2] * 1.4):int(nose[0, 0] + (nose[0, 2] * (3.2)))]
+            brighto = np.mean(imageface)
+            contrasttime = np.sort(np.reshape(imageface, [1, imageface.size]))
+            consize = contrasttime.size
+            facelist.append(onlyjpegs[current])
+            facecaught.append(onlyjpegs[current])
+            face_cascade = cv2.CascadeClassifier(join(script_dir, 'important/haarcascade_frontalface_default.xml'))
+        else:
+            brighto = 404
+            consize = 404
+            nofind.append(onlyjpegs[current])
+            face_cascade = cv2.CascadeClassifier(join(script_dir, 'important/haarcascade_frontalface_default.xml'))
     bigcontrol = (np.mean(contrasttime[0,round(consize-(consize * senso)):consize] - np.mean(contrasttime[0,0:round((consize * senso))])))
     contra.append(bigcontrol)
     brightlist.append(brighto)
 
-#compensate for background color in images where no face was found
-print("\nface detected brightness was", np.mean(facebrightness))
-print("brightness of non-face images was ",np.mean(squarebrightness))
-compensate = np.mean(facebrightness)-np.mean(squarebrightness)
-print("The difference is ",compensate)
-if len(squarebrightness) > 5 and abs(np.mean(facebrightness)-np.mean(squarebrightness)) > 1.5:
-    for current in range(len(squarebrightness)):
-        brightlist[brightlist.index(nofind[current])] = (squarebrightness[current] + (compensate * 0.9))
-    print("Non-face images compensated to ",np.mean(squarebrightness),"\n")
-else:
-    print("Difference not compensated\n")
+#move unsorted images into unsorted folder
+for unsorted in nofind:
+    jpegindex = onlyjpegs.index(unsorted)
+    shutil.move(join(rootpath,unsorted), join(rootpath, "Unsorted"))
+    del brightlist[jpegindex]
+    del contra[jpegindex]
+    del onlyjpegs[jpegindex]
 
-#identify brightest image, sort it, and remove it from consideration (to avoid greycard skewing results)
+#identify brightest image
 brightest = brightlist.index(max(brightlist))
-shutil.move(join(rootpath, onlyjpegs[brightest]), join(rootpath, "Bright"))
 print(onlyjpegs[brightest], "(",brightest,") was the brightest image.\n")
-del brightlist[brightest]
-del onlyjpegs[brightest]
-del contra[brightest]
 
 #report brightness range and success of face detection
 print(min(brightlist),", ",max(brightlist),", ",np.mean(brightlist))
-print(len(facelist)," faces found out of ",len(onlyjpegs)," images total.")
-print("Faces were not found in the following images:")
-print(nofind)
+print(len(facelist)," faces found out of ",len(onlyjpegs)+len(nofind)," images total.")
+print("There were ",len(facecaught)," faces caught by the backup algorithm:")
+if len(facecaught) > 0:
+    print(facecaught)
+if len(nofind) > 0:
+    print("Faces were not found in the following images:")
+    print(nofind)
 
 #calculate threshold for brightness categories based on overall gamut
 mini = min(brightlist) + (darkbasket * (max(brightlist)-min(brightlist)))
@@ -136,4 +149,4 @@ for decide in range(len(brightlist)):
     if brightlist[decide] > maxi:
         shutil.move(join(rootpath, onlyjpegs[decide]), join(rootpath, "Bright"))
 
-cv2.waitKey(0)
+input("\nPress Enter to continue...")
